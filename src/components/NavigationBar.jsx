286c5e70f0f63e88f2550f19c9bff8e27f248bb1
Alfred Githinji <alfredgithinji87@gmail.com>
2023-01-04 01:24:03 +0300

    created grids and navigation bar

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavigationBar = () => {
+  return (
+    <div className="flex w-full bg-primary align-items-center justify-content-around">
+      <div className="brand">Brand</div>
+      <div className="navigation">Navigation Bar</div>
+      <div className="ctaButtons">CTA Buttons</div>
+    </div>
+  );
+};
+
+export default NavigationBar;
