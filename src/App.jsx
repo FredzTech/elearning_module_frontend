ea0c84ed6ec4a395108527de909f8f14c56aa970
Alfred Githinji <alfredgithinji87@gmail.com>
2023-01-04 13:46:55 +0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import "./App.css";
 import { NavigationBar, HomePageGrid } from "./components";
 
 function App() {
+  const text = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.`;
+  console.log(text);
+
   return (
     <div className="App flex flex-col items-center justify-center gap-5">
       <NavigationBar />
