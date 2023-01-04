fc287bb9febb6af14a8267f845d93faa033fb078
Alfred Githinji <alfredgithinji87@gmail.com>
2023-01-04 04:18:01 +0300

    Added card ,grids and resolved a conflict.

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-
+import { NavBtn } from "../components";
 const NavigationBar = () => {
   return (
-    <div className="flex w-full bg-primary align-items-center justify-content-around">
-      <div className="brand">Brand</div>
-      <div className="navigation">Navigation Bar</div>
-      <div className="ctaButtons">CTA Buttons</div>
+    // We can utilize the gap property with flexbox.
+    <div className="flex bg-primary text-plain items-center w-full p-1">
+      <div className="brand mr-auto">
+        <NavBtn name="brand" />
+      </div>
+      <div className="navigation mx-auto my-auto">
+        <NavBtn name="About Us" />
+        <NavBtn name="Courses" />
+      </div>
+      <div className="CTAButtons ml-auto">
+        <NavBtn name="Login" border="true" background="white" />
+        <NavBtn name="Register" border="true" />
+      </div>
     </div>
   );
 };
