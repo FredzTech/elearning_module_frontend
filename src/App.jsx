7680c913d764cc91b7fbfbd8d8cf497620b1d013
Alfred Githinji <alfredgithinji87@gmail.com>
2023-01-04 12:49:41 +0300

    Reconstructed the homepage card

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import { NavigationBar, HomePageCard } from "./components";
 
--- src/components/HomePageCard.jsx
+++ src/components/HomePageCard.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import cardImage from "../assets/course-display.png";
 const HomePageCard = () => {
-  return <div>HomePageCard</div>;
+  return (
+    <div class="w-72 rounded shadow-md debug">
+      <img class="w-full" src={cardImage} alt="Course Card" />
+      <div class="px-6 py-4">
+        <div class="font-bold text-lg mb-2">Discrete Mathematics</div>
+        <p class="text-gray-700 text-base w-full truncate">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
+          quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
+          nihil.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default HomePageCard;
