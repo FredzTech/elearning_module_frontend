03bbcb900eb552e623d9dda64a5e7c6521fc44c3
Alfred Githinji <alfredgithinji87@gmail.com>
2023-01-06 15:44:25 +0300

    Final touches

--- src/components/NavBtn.jsx
+++ src/components/NavBtn.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NavBtn = ({ border, name,goto, background }) => {
+const NavBtn = ({ border, name, goto, background }) => {
   // We shall add the link later.
   return (
-    <button
-      className={`${
-        border === "true"
-          ? " border-2 border-plain text-plain hover:bg-plain hover:text-black"
-          : " border-none text-plain hover:bg-plain hover:text-black"
-      } ${
-        background === "white"
-          ? "bg-white text-black hover:bg-primary hover:text-plain"
-          : ""
-      } px-10 py-3 mx-2 my-2 capitalize `}
-    >
-      <Link to={goto}>{name}</Link>
-      
-    </button>
+    <Link to={goto}>
+      <button
+        className={`${
+          border === "true"
+            ? " border-2 border-plain text-plain hover:bg-plain hover:text-primary"
+            : " border-none text-plain hover:bg-plain hover:text-primary"
+        } ${
+          background === "white"
+            ? "bg-white text-black hover:bg-primary hover:text-primary"
+            : ""
+        } px-10 py-3 mx-2 my-2 capitalize `}
+      >
+        {name}
+      </button>
+    </Link>
   );
 };
 
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import { NavBtn } from "../components";
 
-
 const NavigationBar = () => {
   return (
     // We can utilize the gap property with flexbox.
     <header className="flex bg-primary text-plain items-center w-full p-1">
       <div className="brand mr-auto">
-        <NavBtn name="brand" />
+        <NavBtn name="Home" goto="/" />
       </div>
       <div className="navigation mx-auto my-auto">
-        <NavBtn name="About Us" goto="about" />
-        <NavBtn name="Courses" goto="courses"/>
+        <NavBtn name="Courses" goto="courses" />
+        <NavBtn name="Units" goto="units" />
       </div>
       <div className="CTAButtons ml-auto">
-        <NavBtn name="Login" border="true" goto="login"/>
-        <NavBtn name="Register" border="true" goto="register"/>
+        <NavBtn name="Login" border="true" goto="login" />
+        <NavBtn name="Register" border="true" goto="register" />
       </div>
     </header>
   );
