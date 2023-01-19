ed8a4a245284ce967a849b0306347cf3c55149a8
Alfred Githinji <alfredgithinji87@gmail.com>
2023-01-19 20:35:44 +0300

    Added sth

--- src/components/modals/ModalProvider.jsx
+++ src/components/modals/ModalProvider.jsx
@@ -2,37 +2,30 @@ import React, { createContext, useState } from "react";
 import "./modal.css";
 import { useNavigate } from "react-router";
 
-
-
-
 export const ModalContext = createContext();
 
 // export function openModal(){
 //   return useContext(ModalContext)
 // }
 
+export default function ModalProvider({ children }) {
+  const navigate = useNavigate();
 
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
 
-export default function ModalProvider ({children}){
-  const navigate = useNavigate()
- 
-  const [isOpen , setIsOpen] = useState(false);
-  const [modalContent , setModalContent] = useState(null);
-
-  const openModal =(content)=>{
+  const openModal = (content) => {
     setIsOpen(true);
     setModalContent(content);
   };
   const closeModal = () => {
     setIsOpen(false);
     setModalContent(null);
-    
-     navigate(-1);
-    setTimeout(() => {
 
+    navigate(-1);
+    setTimeout(() => {
       window.location.reload(false);
     }, 100);
-   
   };
 
   if (isOpen) {
