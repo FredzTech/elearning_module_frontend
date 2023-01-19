a40ec0a205d2d3788b63323c0df5fea0831f9242
lawrenceegr <lawrencelangatmi@gmail.com>
2023-01-19 21:00:13 +0300

    modified the stylings of the landing page

--- src/components/NavigationBar/Navbar.jsx
+++ src/components/NavigationBar/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = ({content}) => {
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              {!isAuthenticated? <Link className="text-white text-1xl p-2 radius-md hover:bg-grey"  to="register"> Register</Link> :
+              {!isAuthenticated? <Link className="text-white text-1xl p-2 radius-lg hover:bg-black"  to="register"> Register</Link> :
                 <button
                   type="button"
                   className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -89,7 +89,7 @@ const Navbar = ({content}) => {
                 </button>
                }
                 {/* Profile dropdown */}
-                {!isAuthenticated?  <Link className="text-white text-1xl p-2 radius-md hover:bg-grey"  to="login">Login</Link>  :
+                {!isAuthenticated?  <Link className="text-white text-1xl p-2 radius-lg hover:bg-black"  to="login">Login</Link>  :
                 <Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
