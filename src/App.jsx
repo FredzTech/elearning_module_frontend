511e705b22e634f2b4dd3b6724dce499c3d919b9
Alfred Githinji <alfredgithinji87@gmail.com>
2023-01-24 19:04:03 +0300

    More merging

--- src/App.jsx
+++ src/App.jsx
@@ -49,60 +49,6 @@ function App() {
   //   tutor:"2002",
   // }
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col w-screen h-full pb-2">
-      <AuthContextProvider>
-        <Navbar className="flex" />
-        {/* <div className=" flex flex-1 "> */}
-          <ModalProvider>
-            <IdleTimer>
-              <Routes>
-                <Route exact path="/" element={<HomePage />}></Route>
-                <Route exact path="/pricing" element={<PricingPage />}></Route>
-                <Route exact path="*" element={<NotFound />}></Route>
-                <Route path="student-login" element={<StudentLoginForm />}></Route>
-                <Route path="tutor-login" element={<TutorLoginForm />}></Route>
-                <Route path="admin-login" element={<AdminLoginPage />}></Route>
-                <Route path="register" element={<StudentRegistrationForm/>}></Route>
-                <Route exact path="/units" element={<ChapterPage />}></Route>
-                <Route exact path="/unit" element={<UnitPage />}></Route>
-                <Route exact path="/courses" element={<UnitsPage />}></Route>
-               
-                {/* admin routes */}
-                <Route exact path="/admin" element={<AdminRoute />}>
-                  <Route exact path="users" element={<Users />} />
-                  <Route exact path="dashboard" element={<AdminDashboard />} />
-                  <Route exact path="forbidden" element={<Forbidden />} />
-                  <Route exact path="course" element={<CourseForm />} />
-                  <Route exact path="unitForm" element={<UnitForm />} />
-                  <Route exact path="tutor-reg" element={<TutorRegistrationForm />} />
-                  <Route
-                    exact
-                    path="dashboard"
-                    element={<AdminDashboard />}
-                  ></Route>
-                  <Route
-                    exact
-                    path="students"
-                    element={<StudentsPageAdmin />}
-                  ></Route>
-                  <Route
-                    exact
-                    path="tutors"
-                    element={<TutorsPageAdmin />}
-                  ></Route>
-                </Route>
-
-              {/* tutor routes */}
-              <Route exact path="/tutor" element={<TutorRootPage />}>
-                <Route exact path="dashboard" element={<TutorPage />} />
-                <Route exact path="units" element={<TutorUnitsPage />} />
-                <Route exact path="chapter" element={<ChapterForm />} />
-                <Route exact path="lesson" element={<LessonForm />} />
-                <Route exact path="resources" element={<ResourcesForm />} />
-              </Route>
-              <Route exact path="/tutor/unit" element={<TutorUnitPage />} />
-=======
     <div className="flex flex-col w-screen h-full ">
       <ModalProvider>
         {/* <IdleTimer> */}
@@ -175,7 +121,6 @@ function App() {
               <Route exact path="resources" element={<ResourcesForm />} />
             </Route>
           </Route>
->>>>>>> polish_2
 
           <Route element={<RequireAuth allowedRoles={[2002]} />}>
             <Route exact path="/tutor/unit" element={<TutorUnitPage />} />
