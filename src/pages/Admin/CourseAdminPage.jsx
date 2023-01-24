666fde596956981fbb664a605c00b65735cb0d28
lawrenceegr <lawrencelangatmi@gmail.com>
2023-01-24 20:29:51 +0300

    added navigation courseform

--- src/pages/Admin/CourseAdminPage.jsx
+++ src/pages/Admin/CourseAdminPage.jsx
@@ -1,8 +1,11 @@
 import axios from "../../axios";
-import React, { useEffect, useState } from "react";
-import { NavigateBtn, CourseAdminCard } from "../../components";
+import React, { useEffect, useState ,useContext} from "react";
+import { NavigateBtn, CourseAdminCard, CourseForm } from "../../components";
+import { ModalContext } from "../../components/modals/ModalProvider";
 
 const CoursesAdminPage = () => {
+  const{ openModal, isOpen} = useContext(ModalContext);
+
   const [coursesData, setCoursesData] = useState([]);
   useEffect(() => {
     const fetchCoursesData = async () => {
@@ -22,10 +25,12 @@ const CoursesAdminPage = () => {
     <div className="flex flex-col align-center relative shadow-md sm:rounded-lg w-full h-full pt-2 px-4">
       <div className="flex w-full items-center justify-end mb-3 pr-1">
         <NavigateBtn
-          destination="/admin/course-form"
+          // destination="/admin/course-form"
+          action={openModal}
           text="New Course"
           icon="tenantIcon"
         />
+        {isOpen && <CourseForm/>}
       </div>
       <div className="grid grid-cols-4 gap-5">
         {coursesData.map((course, index) => {
