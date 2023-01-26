dbd02732e0410faad7fff10bbcfdd04aa288b354
Alfred Githinji <alfredgithinji87@gmail.com>
2023-01-26 15:49:41 +0300

    Sidenav functionality achieved successfully.

--- src/components/SideNav/Accordion.jsx
+++ src/components/SideNav/Accordion.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { AiOutlineAppstore } from "react-icons/ai";
 import AccordionItem from "./AccordionItem";
 import { faqs } from "./AccordionData";
-import "./AccordionStyling.css";
 
 const Accordion = () => {
   const [clicked, setClicked] = useState("0");
@@ -14,7 +14,13 @@ const Accordion = () => {
   };
 
   return (
-    <ul className="accordion">
+    <div className="w-full h-full bg-slate-200 m-1 rounded-lg list-none">
+      <div className="w-full px-2 py-2 font-bold text-lg text-slate-200 bg-primary text-center flex justify-start items-center gap-4 rounded-t-lg">
+        <span className="text-white text-3xl">
+          <AiOutlineAppstore />
+        </span>
+        <h1>DISCRETE MATHEMATICS</h1>
+      </div>
       {faqs.map((faq, index) => (
         <AccordionItem
           key={index}
@@ -23,7 +29,7 @@ const Accordion = () => {
           active={clicked === index}
         />
       ))}
-    </ul>
+    </div>
   );
 };
 
