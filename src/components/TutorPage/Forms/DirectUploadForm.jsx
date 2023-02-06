b2cc8144396c7bef23099a2fa5ad910b71bc7e76
Alfred Githinji <alfredgithinji87@gmail.com>
2023-02-06 23:49:40 +0300

    File upload from front-end working correctly.

--- src/components/TutorPage/Forms/DirectUploadForm.jsx
+++ src/components/TutorPage/Forms/DirectUploadForm.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
-import { CustomNav, Button } from "../../../components";
+import { CustomNav, Button, AlertBox } from "../../../components";
 import axios from "../../../axios";
 import { useNavigate } from "react-router-dom";
 const DirectUploadForm = () => {
   const navigate = useNavigate();
   // DECLARATION OF VARIABLES
   //=========================
-  const [file, setFile] = useState();
-  const [courseTitle, setCourseTitle] = useState("");
-  const [signedUrl, setSignedUrl] = useState(null);
-  const [key, setKey] = useState(null);
+  const [file, setFile] = useState(null);
+  const [courseTitle, setCourseTitle] = useState(null);
 
+  const [response, setResponse] = useState(null);
+  const [responseTracker, setResponseTracker] = useState(null);
+  const [statusTracker, setStatusTracker] = useState(null);
   async function getSignedUrl({ file }) {
     // Generating our req body.
     const formData = new FormData();
@@ -30,9 +31,7 @@ const DirectUploadForm = () => {
       console.log(`Signed url ${JSON.stringify(data)}, ${urlCreationStatus}`);
       const { signedUrl: url, Key } = data;
       console.log(`Destructured data ${url}, ${Key}`);
-      setSignedUrl(url);
-      setKey(Key);
-      return urlCreationStatus;
+      return { urlCreationStatus, Key, url };
     } catch (err) {
       console.log("Something went wrong when creating the signed url");
       console.log(err);
@@ -40,7 +39,7 @@ const DirectUploadForm = () => {
     }
   }
 
-  async function uploadFile({ file }) {
+  async function uploadFile({ file, url: signedUrl }) {
     // We upload file directly to s3
     const config = {
       headers: { "Content-Type": "multipart/form-data" },
@@ -65,40 +64,54 @@ const DirectUploadForm = () => {
     }
   }
 
-  const savingFileToDB = async (urlCreationStatus) => {
+  const savingFileToDB = async ({ urlCreationStatus, Key, url }) => {
     if (urlCreationStatus === 201) {
       // Uploading file to server.
       try {
-        const uploadResponse = await uploadFile({ file: file });
+        const uploadResponse = await uploadFile({ file: file, url: url });
         let uploadResponseStatus = uploadResponse.status;
         console.log(`File upload status ${uploadResponseStatus}`);
         if (uploadResponseStatus === 200) {
           // Saving data to the status
           console.log("All is a go. We can save coursedata to the DB");
-          console.log(`file Name to be stored ${key}`);
+          console.log(`file Name to be stored ${Key}`);
           let courseData = {
             courseTitle,
-            courseImage: key,
+            courseImage: Key,
           };
 
           const config = {
             headers: { "Content-Type": "application/json" },
           };
 
-          const saveCourse = await axios.post(
+          const response = await axios.post(
             "/course/new-course",
             courseData,
             config
           );
-          console.log(
-            `Course Data saved successfully ${JSON.stringify(saveCourse)} `
-          );
+
+          const { status } = response;
+
+          if (status === 201) {
+            setResponse("Data saved successfully to DB.");
+            setStatusTracker(true);
+            setResponseTracker(true);
+            setTimeout(() => {
+              setResponseTracker(false);
+            }, 2500);
+          }
         }
       } catch (err) {
-        console.log(
-          "Error occurred while trying to save course data to the db"
-        );
-        console.log(err);
+        if (err.message === "Request failed with status code 409") {
+          setResponse("This record already exists.");
+          setStatusTracker(false);
+          setResponseTracker(true);
+          setTimeout(() => {
+            setResponseTracker(false);
+          }, 2500);
+        } else {
+          console.log(err);
+        }
       }
     }
   };
@@ -111,13 +124,31 @@ const DirectUploadForm = () => {
     setFile(file);
   };
 
+  const validateForm = () => {
+    if (courseTitle !== null && file !== null) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   const fileUploadHandler = async (e) => {
     // Prevents default behaviour of our form
     e.preventDefault();
     // Fetches a signedUrl
-    const urlCreationStatus = await getSignedUrl({ file: file });
-    console.log(`url creation status ${urlCreationStatus}`); //Always comes through for us.
-    savingFileToDB(urlCreationStatus);
+    const validation = validateForm();
+    if (validation == true) {
+      const urlCreationStatus = await getSignedUrl({ file: file });
+      savingFileToDB(urlCreationStatus);
+    } else {
+      console.log("Error!Error during validation");
+      setResponse("Kindly fill all details correctly.");
+      setStatusTracker(false);
+      setResponseTracker(true);
+      setTimeout(() => {
+        setResponseTracker(false);
+      }, 2500);
+    }
   };
 
   return (
@@ -125,6 +156,11 @@ const DirectUploadForm = () => {
       <div className="flex flex-col justify-center items-center debug w-2/5">
         <CustomNav text="DIRECT UPLOAD FORM" />
         <form encType="multipart/form-data" className="form-styling">
+          <AlertBox
+            responseTracker={responseTracker}
+            statusTracker={statusTracker}
+            response={response}
+          />
           <div className="input-wrap">
             <label htmlFor="course" className="w-full ">
               Course Details
@@ -163,21 +199,23 @@ const DirectUploadForm = () => {
               alt="image"
             /> */}
 
-            {/* <video
+            <video
               className=" mt-2 w-[850px]  h-auto flex rounded-lg shadow-xl shadow-slate-500 "
               src="http://localhost:4000/s3Direct/45b1f269541bf014a6ebc76666f22590.mp4"
               poster="http://localhost:4000/s3Direct/8614f51246cde029ea9e432be8b9ff5d.png"
               controls
             >
               This video is not supported by your browser.
-            </video> */}
+            </video>
           </div>
           {/* CTA BUTTONS */}
           <div className="cta-wrap">
             <Button
               type="button"
               text="Add Course"
-              onClick={fileUploadHandler}
+              onClick={(e) => {
+                fileUploadHandler(e);
+              }}
             />
           </div>
         </form>
